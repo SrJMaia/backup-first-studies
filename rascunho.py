cf1ca9a1532d76df1400221b89546021a518bb99
SrJMaia <66738508+SrJMaia@users.noreply.github.com>
2021-05-27 09:12:58 +0100

    Backtest novo
    Adicionado single_backtest

--- rascunho.py
+++ rascunho.py
@@ -1,28 +1,9 @@
-import numpy as np
-from numba import njit
-
-@njit(fastmath=True)
-def finance_calculation(balance, saldo_inicial, saldo_final, preco_eur, eur):
-
-    initial_balance = 1000
-    lot = 1000
-
-    #if balance > (3*initial_balance):
-    #    lot = (balance - initial_balance) // 1000 * 1000
-
-    comission = (lot//1000) * 0.07
-    #overnight = (lot//1000) * 0.1
-    # random entre -2 a 2 pips para simular slippage
-
-    if eur:
-        tot = (lot * (saldo_inicial - saldo_final)) / saldo_final
-        tot2 = tot - comission
-        return (tot2 + balance), tot2
-    else:
-        tot = (lot * (saldo_inicial - saldo_final)) / saldo_final
-        tot2 = tot / saldo_final / preco_eur - comission
-        return (tot2 + balance), tot2
-
+"""
+Crio uma coluna nova com o timeframe que sera testado e os respectivos preços
+coluna OHLC sera para testar o TPSL
+um unico loop onde, quando nao for nan e flag abrir operacao == True, abre operacao
+else, apenas passa pelos preços OHLC
+"""
 
 @njit(parallel=True)
 def single_backtest(array, multiply_tp, multiply_sl, eur = True, balance = 1000):
@@ -76,40 +57,39 @@ def single_backtest(array, multiply_tp, multiply_sl, eur = True, balance = 1000)
 
     for i in np.arange(array[0].size): # Array tamanho
 
-        if not np.isnan(array[4][i]):
-            if array[7][i] == 1. and buy_sell[0] == 1.: #Compra
-                operacao_info[0] = array[4][i]
-                buy_sell[0] = False
-                if eur:
-                    operacao_info[4] = array[4][i] + round(multiply_tp * array[5][i],3) # TP
-                    #operacao_info[4] = array[0][i] + tk_jpy
-                    operacao_info[5] = array[4][i] - round(multiply_sl * array[5][i],3) # SL
-                    #operacao_info[5] = array[0][i] - sl_jpy
-                else:
-                    operacao_info[4] = array[4][i] + round(multiply_tp * array[5][i],5) # TP
-                    #operacao_info[4] = array[0][i] + tk_normal
-                    operacao_info[5] = array[4][i] - round(multiply_sl * array[5][i],5) # SL
-                    #operacao_info[5] = array[0][i] - sl_normal
-            elif array[6][i] == 1. and buy_sell[1] == 1.:
-                operacao_info[1] = array[4][i]
-                buy_sell[1] = False
-                if eur:
-                    operacao_info[2] = array[4][i] - round(multiply_tp * array[5][i],3) # TP
-                    #operacao_info[2] = array[0][i] - tk_jpy
-                    operacao_info[3] = array[4][i] + round(multiply_sl * array[5][i],3) # SL
-                    #operacao_info[3] = array[0][i] + sl_jpy
-                else:
-                    operacao_info[2] = array[4][i] - round(multiply_tp * array[5][i],5) # TP
-                    #operacao_info[2] = array[0][i] - tk_normal
-                    operacao_info[3] = array[4][i] + round(multiply_sl * array[5][i],5) # SL
-                    #operacao_info[3] = array[0][i] + sl_normal
+        if array[7][i] and buy_sell[0] and array[4][i] != np.nan: #Compra
+            operacao_info[0] = array[4][i]
+            buy_sell[0] = False
+            if eur:
+                operacao_info[4] = array[4][i] + round(multiply_tp * array[5][i],3) # TP
+                #operacao_info[4] = array[0][i] + tk_jpy
+                operacao_info[5] = array[4][i] - round(multiply_sl * array[5][i],3) # SL
+                #operacao_info[5] = array[0][i] - sl_jpy
+            else:
+                operacao_info[4] = array[4][i] + round(multiply_tp * array[5][i],5) # TP
+                #operacao_info[4] = array[0][i] + tk_normal
+                operacao_info[5] = array[4][i] - round(multiply_sl * array[5][i],5) # SL
+                #operacao_info[5] = array[0][i] - sl_normal
+        elif array[6][i] and buy_sell[1] and array[4][i] != np.nan:
+            operacao_info[1] = array[4][i]
+            buy_sell[1] = False
+            if eur:
+                operacao_info[2] = array[4][i] - round(multiply_tp * array[5][i],3) # TP
+                #operacao_info[2] = array[0][i] - tk_jpy
+                operacao_info[3] = array[4][i] + round(multiply_sl * array[5][i],3) # SL
+                #operacao_info[3] = array[0][i] + sl_jpy
+            else:
+                operacao_info[2] = array[4][i] - round(multiply_tp * array[5][i],5) # TP
+                #operacao_info[2] = array[0][i] - tk_normal
+                operacao_info[3] = array[4][i] + round(multiply_sl * array[5][i],5) # SL
+                #operacao_info[3] = array[0][i] + sl_normal
 
         check_sell_sl = (array[0][i] >= operacao_info[3]) or (array[1][i] >= operacao_info[3]) or (array[2][i] >= operacao_info[3]) or (array[3][i] >= operacao_info[3])
         check_sell_buy = (array[0][i] <= operacao_info[2]) or (array[1][i] <= operacao_info[2]) or (array[2][i] <= operacao_info[2]) or (array[3][i] <= operacao_info[2])
         check_buy_sl = (array[0][i] <= operacao_info[5]) or (array[1][i] <= operacao_info[5]) or (array[2][i] <= operacao_info[5]) or (array[3][i] <= operacao_info[5])
         check_buy_tp = (array[0][i] >= operacao_info[4]) or (array[1][i] >= operacao_info[4]) or (array[2][i] >= operacao_info[4]) or (array[3][i] >= operacao_info[4])
 
-        if np.isnan(array[4][i]):
+        if array[4][i] != np.nan:
             if buy_sell[0] == False and check_buy_sl:
                 balance_backtest, buy_result = finance_calculation(balance=balance_backtest, saldo_inicial=operacao_info[5],
                                                                    saldo_final=operacao_info[0], eur = eur,
@@ -119,6 +99,7 @@ def single_backtest(array, multiply_tp, multiply_sl, eur = True, balance = 1000)
                 buy_sell_tot_trades_indi[0] += 1
                 buy_sell_tot_trades_indi[2] += 1
                 buy_sell[0] = True
+                break
             elif buy_sell[0] == False and check_buy_tp:
                 balance_backtest, buy_result = finance_calculation(balance=balance_backtest, saldo_inicial=operacao_info[4],
                                                                    saldo_final=operacao_info[0], eur = eur,
@@ -128,6 +109,7 @@ def single_backtest(array, multiply_tp, multiply_sl, eur = True, balance = 1000)
                 buy_sell_tot_trades_indi[0] += 1
                 buy_sell_tot_trades_indi[2] += 1
                 buy_sell[0] = True
+                break
 
             if buy_sell[1] == False and check_sell_sl:
                 balance_backtest, sell_result = finance_calculation(balance=balance_backtest,saldo_inicial=operacao_info[1],
@@ -138,6 +120,7 @@ def single_backtest(array, multiply_tp, multiply_sl, eur = True, balance = 1000)
                 buy_sell_tot_trades_indi[1] += 1
                 buy_sell_tot_trades_indi[2] += 1
                 buy_sell[1] = True
+                break
             elif buy_sell[1] == False and check_sell_buy:
                 balance_backtest, sell_result = finance_calculation(balance=balance_backtest, saldo_inicial=operacao_info[1],
                                                                     saldo_final=operacao_info[2], eur = eur,
@@ -147,5 +130,6 @@ def single_backtest(array, multiply_tp, multiply_sl, eur = True, balance = 1000)
                 buy_sell_tot_trades_indi[1] += 1
                 buy_sell_tot_trades_indi[2] += 1
                 buy_sell[1] = True
+                break
 
     return tot_trades, sell_orders, buy_orders
